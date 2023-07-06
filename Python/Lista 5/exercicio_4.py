7adf196686d891ec345721d3495bdbf0db3f93bd
erick-santos-8 <137582518+erick-santos-8@users.noreply.github.com>
2023-07-06 16:47:45 -0300

    Update exercicio_4.py

--- Python/Lista 5/exercicio_4.py
+++ Python/Lista 5/exercicio_4.py
@@ -1,28 +1,20 @@
 from pythonds.graphs import *
 
-# populacao = Graph()
-# pessoa1 = populacao.addVertex(1)
-# pessoa1.addNeighbor(3)
-# pessoa2 = populacao.addVertex(2)
-# pessoa2.addNeighbor(3)
-# pessoa3 = populacao.addVertex(3)
-
 class Descobrir:
     def __init__(self, num):
         self.populacao = Graph()
-        self.pessoas = []
         self.lista_confianca = {}
         for i in range(1, num+1):
-            self.pessoas.append(self.populacao.addVertex(i))
+            self.populacao.addVertex(i)
             self.lista_confianca[i] = 0
     def descobrir_Juiz(self, lista: list):
         for i in range(len(lista)):
-            self.pessoas[lista[i][0]-1].addNeighbor(lista[i][1])
+            self.populacao.getVertex(lista[i][0]).addNeighbor(lista[i][1])
             self.lista_confianca[lista[i][1]] += 1
-        
+            
         for i in range(1, len(self.lista_confianca)+1):
             if self.lista_confianca[i] == self.populacao.numVertices - 1:
-                if len(self.pessoas[i-1].getConnections()) == 0:
+                if len(self.populacao.getVertex(i).getConnections()) == 0:
                     return i
         return -1
     
